e0ee9202eaf78a0eea2af70357b9133d24571f21
Ivan Ashikhmin <sushkoos@gmail.com>
2022-12-15 18:39:36 +0400

    Seminar 3 hw tasks

--- Seminar_3/HW3/src/task3_stream.java
+++ Seminar_3/HW3/src/task3_stream.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class task3 {
+public class task3_stream {
     public static void main(String[] args) {
         ThreadLocalRandom randomNum = ThreadLocalRandom.current();
         Scanner scanner = new Scanner(System.in);
@@ -15,14 +15,12 @@ public class task3 {
         for (int i = 0; i < size; i++) {
             arr.add(randomNum.nextInt(0, 10));
         }
-        int sum = 0;
-        for (int i : arr){
-            sum += i;
-        }
         System.out.printf("Array: %s\n", arr);
         System.out.printf("Minimum: %s\nMaximum: %s\nMean: %s",
                 Collections.min(arr),
                 Collections.max(arr),
-                (double) sum / size);
+                arr.stream()
+                        .mapToDouble(a -> a)
+                        .sum() / size);
     }
 }
